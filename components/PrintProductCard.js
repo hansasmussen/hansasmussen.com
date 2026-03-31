fa958aa937bc8c9d17a0720dad0ade1ab247b9bd
Hans Asmussen <hansasmussen@MacStudendeHans.localdomain>
2026-03-31 10:08:53 +0200

    Adjust print mockups by orientation

--- components/PrintProductCard.js
+++ components/PrintProductCard.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function formatPrice(price) {
   return new Intl.NumberFormat("en-DK", {
@@ -10,11 +10,38 @@ function formatPrice(price) {
   }).format(price);
 }
 
+function useImageRatio(src) {
+  const [ratio, setRatio] = useState(1);
+
+  useEffect(() => {
+    let isCancelled = false;
+    const image = new Image();
+
+    image.onload = () => {
+      if (isCancelled) return;
+      const nextRatio = image.naturalWidth / Math.max(image.naturalHeight, 1);
+      setRatio(nextRatio || 1);
+    };
+
+    image.src = src;
+
+    return () => {
+      isCancelled = true;
+    };
+  }, [src]);
+
+  return ratio;
+}
+
 function MockupSlide({ src, alt }) {
+  const ratio = useImageRatio(src);
+  const orientationClass =
+    ratio < 0.92 ? "is-portrait" : ratio > 1.08 ? "is-landscape" : "is-square";
+
   return (
-    <div className="print-mockup">
+    <div className={`print-mockup ${orientationClass}`}>
       <div className="print-mockup-room" />
-      <div className="print-mockup-frame">
+      <div className="print-mockup-frame" style={{ "--print-ratio": ratio }}>
         <div className="print-mockup-mat">
           <img src={src} alt={alt} loading="lazy" />
         </div>
